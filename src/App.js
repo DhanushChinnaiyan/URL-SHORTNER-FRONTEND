181b26abccb3d10cc82261dcbc0db683afe4e14c
DhanushChinnaiyan <dhanushmd4021@gmail.com>
2023-04-29 20:35:55 +0530

    No Page added

--- src/App.js
+++ src/App.js
@@ -1,75 +1,76 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Route, Switch, useHistory } from 'react-router-dom';
-import { decodeToken } from 'react-jwt';
-import Login from './Components/UserEntry/Login';
-import Signup from './Components/UserEntry/Signup';
-import Forgot from './Components/UserEntry/Forgot/Forgot';
-import MainDash from './Components/DashBoard/MainDash/MainDash';
-import AccountActivation from './Components/UserEntry/AccountActivation';
-import ResetPassword from './Components/UserEntry/Forgot/ResetPassword';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { decodeToken } from "react-jwt";
+import Login from "./Components/UserEntry/Login";
+import Signup from "./Components/UserEntry/Signup";
+import Forgot from "./Components/UserEntry/Forgot/Forgot";
+import MainDash from "./Components/DashBoard/MainDash/MainDash";
+import AccountActivation from "./Components/UserEntry/AccountActivation";
+import ResetPassword from "./Components/UserEntry/Forgot/ResetPassword";
+import Nopage from "./Components/NoPage/Nopage";
 
 function App() {
- 
+  // const history = useHistory()
+  //   useEffect(()=>{
+  //    const getUrls = async() => {
 
-  const history = useHistory()
-//   useEffect(()=>{
-//    const getUrls = async() => {
+  //     // getting token from header
+  //     const token = localStorage.getItem("usertoken")
 
-//     // getting token from header
-//     const token = localStorage.getItem("usertoken")
+  //     // validating the header token
+  //     const user = decodeToken(token)
 
-//     // validating the header token
-//     const user = decodeToken(token)
-  
-//     if(user) { 
-//       history.replace("/")
-//     }
-    
+  //     if(user) {
+  //       history.replace("/")
+  //     }
 
-// // // getting the urls
-// //     try {
-// //       const response = await fetch(`https://url-shortner-backend-itr5.onrender.com/shorturl/all/${user.id}`,{
-// //         method:"GET",
-// //         headers:{
-// //           "user-login-token":localStorage.getItem("usertoken")
-// //         }
-// //       })
-// //      const data = await response.json()
-// //      console.log(data)
+  // // // getting the urls
+  // //     try {
+  // //       const response = await fetch(`https://url-shortner-backend-itr5.onrender.com/shorturl/all/${user.id}`,{
+  // //         method:"GET",
+  // //         headers:{
+  // //           "user-login-token":localStorage.getItem("usertoken")
+  // //         }
+  // //       })
+  // //      const data = await response.json()
+  // //      console.log(data)
 
-// //     } catch (error) {
-// //      console.log("All Url Error",error)
-// //     }
-//    }
+  // //     } catch (error) {
+  // //      console.log("All Url Error",error)
+  // //     }
+  //    }
 
-//    getUrls()
-   
-//   },[])
+  //    getUrls()
+
+  //   },[])
 
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/">
+          <MainDash />
+        </Route>
         <Route path="/login">
-          <Login/>
+          <Login />
         </Route>
         <Route path="/signup">
-          <Signup/>
+          <Signup />
         </Route>
         <Route path="/activate">
-          <AccountActivation/>
+          <AccountActivation />
         </Route>
         <Route path="/forgot">
-          <Forgot/>
+          <Forgot />
         </Route>
         <Route path="/resetpassword">
-          <ResetPassword/>
+          <ResetPassword />
         </Route>
-        <Route exact path="/">
-          <MainDash/>
+
+        <Route path="**">
+          <Nopage />
         </Route>
       </Switch>
-      
     </div>
   );
 }
